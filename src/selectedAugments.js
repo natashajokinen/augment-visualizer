db08af8579a945cd3450d3565a17b0997ddbc1f2
natashajokinen <76977006+natashajokinen@users.noreply.github.com>
2022-01-03 14:37:18 -0500

    feat(selectedAugs): show a warning when multiple selections per category

--- src/selectedAugments.js
+++ src/selectedAugments.js
@@ -1,8 +1,7 @@
-import Checkbox from '@mui/material/Checkbox';
+import Alert from '@mui/material/Alert';
 import Collapse from '@mui/material/Collapse';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { useTheme } from "@mui/material/styles";
 import { createMakeAndWithStyles } from "tss-react/compat";
@@ -15,7 +14,8 @@ const {makeStyles} = createMakeAndWithStyles({useTheme});
 const useStyles = makeStyles()((theme) => ({
   root: {
     width: '350px',
-    float: 'right',
+    float: 'left',
+    paddingLeft: '40px',
   },
   nested: {
     paddingLeft: theme.spacing(4),
@@ -67,7 +67,10 @@ function SelectedAugments() {
       </Collapse>);
   });
   return (
-    <List className={classes.root}>{giantList}</List>
+    <div className={classes.root}>
+      <List>{giantList}</List>
+      {warning ? <Alert variant='filled' severity='warning'>Warning: There are two or more augment selections from the same category</Alert>: ''}
+    </div>
   );
 }
 
